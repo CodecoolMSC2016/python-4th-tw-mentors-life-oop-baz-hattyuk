1d104c8eb524e975ebf399a6eb668f4702d6abd2
Nikolett Uracs <loveninagold@gmail.com>
2017-01-18 11:37:03 +0100

    edited mentors and person.py

--- mentor.py
+++ mentor.py
@@ -1,13 +1,30 @@
 from person import Person
+import csv
 
 
 class Mentor(Person):
 
-
-    def _init__(self, first_name, last_name, year_of_birth, gender, nickname):
+    def _init__(self, nickname):
         super().__init__(first_name, last_name, year_of_birth, gender)
         self.nickname = nickname
 
     @classmethod
-    def create_by_csv(cls, filename):
-        with
+    def create_by_csv(cls):
+        with open('data/mentors.csv') as csvfile:
+            mentors_list = []
+            data = csv.reader(csvfile)
+            for row in data:
+                person = Mentor()
+                person.first_name = row[0]
+                person.last_name = row[1]
+                person.year_of_birth = row[2]
+                person.gender = row[3]
+                person.nickname = row[4]
+                mentors_list.append(person)
+        return mentors_list
+
+
+
+mentor1 = Mentor.create_by_csv()
+
+print(mentor1[0].first_name, mentor1[0].last_name)
