a53d4695b6badd2ea62a030d3974aa92cbdc2f7b
agnes-kabaly <alicet.woods@gmail.com>
2017-01-18 13:34:30 +0100

    raise is missing

--- student.py
+++ student.py
@@ -4,26 +4,28 @@ import csv
 
 class Student(Person):
 
-    def __init__(self, first_name, last_name, year_of_birth, gender, knowledge_level, energy_level):
-        super().__init__(first_name, last_name, year_of_birth, gender)
+    def __init__(self, first_name="", last_name="", year_of_birth="", gender="", knowledge_level=0, energy_level=0):
+        super().__init__()
         self.knowledge_level = knowledge_level
         self.energy_level = energy_level
 
     @classmethod
-    def create_by_csv(cls, csv_file = "data/students.csv"):
+    def create_by_csv(cls):
         student_list = []
-        with open(csv_file, "r") as myfile:
-            for line in myfile.readlines():
-                line = line.strip("\n")
-                line = line.split(", ")
+        with open("data/students.csv") as csvfile:
+            data = csv.reader(csvfile)
+            for row in data:
                 one_student = Student()
-                one_student.first_name = line[0]
-                one_student.last_name = line[1]
-                one_student.year_of_birth = line[2]
-                one_student.gender = line[3]
-                one_student.knowledge_level = int(line[4])
-                one_student.energy_level = int(line[5])
-                students.append(one_student)
-        return student_list
+                one_student.first_name = row[0]
+                one_student.last_name = row[1]
+                one_student.year_of_birth = row[2]
+                one_student.gender = row[3]
+                one_student.knowledge_level = int(row[4])
+                one_student.energy_level = int(row[5])
+                student_list.append(one_student)
+            return student_list
 
-print(Student.create_by_csv)
+
+student1 = Student.create_by_csv()
+
+print(student1[0].first_name, student1[0].last_name, student1[0].year_of_birth)
